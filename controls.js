3142a439172f8b78e965a1ceb6808483fbb36fb2
nhengler <niko_las1934@hotmail.com>
2025-06-30 05:22:11 -0300

    inserção do texto do jornal

--- controls.js
+++ controls.js
@@ -1,13 +1,13 @@
 const projectCards = document.querySelectorAll('.project-card');
 const projectDescriptions = document.querySelectorAll('.description');
 
-// Adiciona o evento de clique a cada cartão de projeto
+// adiciona o evento de clique a cada cartão de projeto
 projectCards.forEach((card, index) => {
   card.addEventListener('click', function () {
     
-    const description = projectDescriptions[index]; // tirar duvida sobre o colchete de index
+    const description = projectDescriptions[index]; 
     
-      if (description.style.maxHeight === '500px') { // testar apenas com 1 if
+      if (description.style.maxHeight === '500px') { 
         
         description.style.maxHeight = '0';
         description.style.opacity = '0';
